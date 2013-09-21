33487ee97b68bf461c8662cb12ebc94676cea8a6
Shawn McFarlane <javaforhire@gmail.com>
2013-09-20 18:25:08 -0700

    update to homepage featured videos - add link and style text callout

--- sites/all/modules/cidesign/featured_videos.tpl.php
+++ sites/all/modules/cidesign/featured_videos.tpl.php
@@ -7,14 +7,14 @@
 ?>
 
 <?php //if ($featured_videos): ?>
-  <?php 
-  for ($i = 0, $n = 1; $i < count($featured_videos); $i++, $n++): 
+  <?php for ($i = 0, $n = 1; $i < count($featured_videos); $i++, $n++): 
   $rec = $featured_videos[$i]; ?>
   <article id="video-<?php print $n; ?>">
-    <?php print $rec['button']; ?>
-    <img src="<?php print $rec['image']; ?>" alt="<?php print $rec['title']; ?>" title="<?php print $rec['title']; ?>" id="featured-video-<?php print $n; ?>" />
-    <span>07.23.13</span>
-    <figcaption><?php print $rec['url']; ?></figcaption>
+    <?php /* print $rec['button']; */ ?>
+    <a href="<?php print url('node/' . $rec['nid']); ?>">
+      <img src="<?php print $rec['image']; ?>" alt="<?php print $rec['title']; ?>" title="<?php print $rec['title']; ?>" id="featured-video-<?php print $n; ?>" />
+    </a>
+    <figcaption><?php print l($rec['title'], 'node/' . $rec['nid']); ?></figcaption>
   </article> 
   <?php endfor; ?>
 <?php //endif; ?>
