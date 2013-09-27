65a7a3f19722c6348277368c39e55bea8fdf40df
Shawn McFarlane <javaforhire@gmail.com>
2013-09-27 10:23:39 -0700

    link featured blocks to their page reference

--- sites/all/modules/cidesign/featured_videos.tpl.php
+++ sites/all/modules/cidesign/featured_videos.tpl.php
@@ -5,16 +5,17 @@
  * 
  */
 ?>
-
 <?php //if ($featured_videos): ?>
   <?php for ($i = 0, $n = 1; $i < count($featured_videos); $i++, $n++): 
-  $rec = $featured_videos[$i]; ?>
+  $rec = $featured_videos[$i];
+  $ref = node_load($rec['nid']);
+  $link_ref = $ref->field_page_reference[LANGUAGE_NONE][0]['nid']; ?>
   <article id="video-<?php print $n; ?>">
     <?php /* print $rec['button']; */ ?>
-    <a href="<?php print url('node/' . $rec['nid']); ?>">
+    <a href="<?php print url('node/' . $link_ref); ?>">
       <img src="<?php print $rec['image']; ?>" alt="<?php print $rec['title']; ?>" title="<?php print $rec['title']; ?>" id="featured-video-<?php print $n; ?>" />
     </a>
-    <figcaption><?php print l($rec['title'], 'node/' . $rec['nid']); ?></figcaption>
+    <figcaption><?php print l($rec['title'], 'node/' . $link_ref); ?></figcaption>
   </article> 
   <?php endfor; ?>
 <?php //endif; ?>
